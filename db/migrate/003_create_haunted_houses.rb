f8c59f9119384638aab189e4d3bf71b5f4a14a15
Andrew Smith <848smith@gmail.com>
2020-04-17 01:59:52 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,9 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string 
+    end
+  end
+end
