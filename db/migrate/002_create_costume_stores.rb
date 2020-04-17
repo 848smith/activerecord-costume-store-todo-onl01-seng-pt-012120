109b7a652fff14c8d31210a29fbb64db1e4a0002
Andrew Smith <848smith@gmail.com>
2020-04-17 02:04:29 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -7,8 +7,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.integer :costume_inventory
       t.integer :employee_count
       t.boolean :in_business
-      t.string :opening_time
-      t.string :closing_time
+      t.datetime :opening_time
+      t.datetime :closing_time
     end
   end
 end
