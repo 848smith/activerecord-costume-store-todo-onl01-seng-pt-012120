7a33a5b043fbbb3972cb0690fb2b9f1c2dfc77f0
Andrew Smith <848smith@gmail.com>
2020-04-17 02:03:07 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -5,8 +5,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.string :name
       t.string :location
       t.string :theme
-      t.integer :price
-      t.
+      t.float :price
+      t.boolean :family_friendly
       t.datetime :opening_date
       t.datetime :closing_date
       t.text :description
